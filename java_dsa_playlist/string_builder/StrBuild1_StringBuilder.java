9f879189dd21cbe8ded858fc7cec3abf5e1290cf
FireStack <113893489+FireStackDev@users.noreply.github.com>
2025-12-27 14:59:45 +0530

    string builder added

---
+++ java_dsa_playlist/string_builder/StrBuild1_StringBuilder.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class StrBuild1_StringBuilder {
+    public static void main(String[] args) {
+        StringBuilder str = new StringBuilder("hello world");
+
+        System.out.println(str);
+    }
+}
